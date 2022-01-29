0262ab424ed0bf1ef227b80e4072beea0a11569e
Advaith Curpod <45689635+advaithcurpod@users.noreply.github.com>
2022-01-29 20:53:24 +0530

    added feature for TxLog & total contribution

--- maNGO.sol
+++ maNGO.sol
@@ -6,7 +6,7 @@ contract NGO {
 
     struct Donor {
         string name;
-        uint donatedAmount;
+        uint totalDonatedAmount;
         address donorAddress;
     }
 
@@ -18,9 +18,35 @@ contract NGO {
 
     //ngo maintains a list of donors
     Donor[] listOfDonors;
+    Donor[] listOfDonorsWithIndividualTx;
 
     function donate(string memory _name) external payable {
+        //increment money in ngo wallet
         ngoWallet[owner] += msg.value;
-        listOfDonors.push(Donor(_name, msg.value, msg.sender))
+
+        //create an instance of the current donor
+        Donor memory currDonor = Donor(_name, msg.value, msg.sender);
+        //add this donors details in the transaction log
+        listOfDonorsWithIndividualTx.push(currDonor);
+
+        //add donor in List of Donors
+        //if already present, increment the contributed value
+        uint256 i;
+        for (i = 0; i < listOfDonors.length; i++) {
+            // if this donor already has done a donation
+            if(currDonor.donorAddress == listOfDonors[i].donorAddress) {
+                currDonor.totalDonatedAmount += msg.value;
+                break;
+            }
+        }
+        if(i == listOfDonors.length) {
+            listOfDonors.push(currDonor);
+        }
     }
+
+    //Transaction Log
+    function transactionLog() public view returns(Donor[] memory) {
+        return listOfDonorsWithIndividualTx;
+    }
+    
 }
